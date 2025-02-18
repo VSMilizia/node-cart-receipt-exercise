1e077a8fac7b4a69ecfc9055b0fe145025e65a14
VSMilizia <vittoria.milizia@gmail.com>
2025-02-18 09:29:44 +0100

    First release

--- cart.mjs
+++ cart.mjs
@@ -1,14 +1,60 @@
 import {carts, products, promoCode, users} from "./dataset.mjs";
 import * as core from "./core-cart.mjs";
+import * as fs from "fs";
 
-console.log('Promo', promoCode);
-console.log('Products', products);
-console.log('Users', users);
-console.log('Cart', carts);
+var dirName = "receipts";
+core.createDir(dirName);
 
-console.log('Discount', core.discountedPrice(100, 0.2));
+for (let cartRow of carts) {                    // -- Cycle all cart in carts
 
-for (let cartRow of carts) {
+    let ticket = [];                            // -- Instantiate ticket
 
-    // genera ricevuta
+    ticket.push(core.generateDecor("+"));
+    ticket.push(core.getHeader());
+    ticket.push(core.getDate());
+    ticket.push(core.generateDecor("*"));       // -- Header end
+
+    let products = cartRow.products;
+                                                
+    let total = products.reduce((acc,product)=>{  // -- Associate ean code with product name and price
+        let productDetails = core.getProduct(product);
+        acc += productDetails[2];               
+        ticket.push("\t" + productDetails.join("\t"));  // -- Meanwhile list the products
+        return acc;
+    },0);
+
+    if (total == 0){                            // -- If the cart is empty continue to next ticket
+        console.log("No products in user:"+cartRow.user);
+        continue;
+    }
+
+    ticket.push(core.generateDecor("*"));
+
+    ticket.push(`\tTotale:\t\t\t\t${total.toFixed(2)}`);    
+
+    ticket.push(core.generateDecor("+"));
+
+    var user = users.find(obj => obj.uuid == cartRow.user); // -- Get the user object
+
+    if (user.promo){                            // -- Insert the discount due to promo code
+        var promo = promoCode.find(obj => obj.name == user.promo);
+        ticket.push(core.generateDiscount(promo,total));
+        ticket.push(core.generateDecor("+"));
+        total = total*(1-promo.percentage);
+    };    
+
+    ticket.push("");                            // -- Add an empty line
+    ticket.push(core.generateDecor("**"));      // -- and decoration
+
+    if(user.wallet<total){                      // -- Check available credit
+        continue;
+    }else{
+        ticket.push(core.creditUpdate(user,total));  
+    }
+    
+    ticket.push(core.generateDecor("**"));      // -- End of ticket
+
+    var ticketName = core.createTicketName(user);
+
+    fs.writeFileSync(`receipts/${ticketName}.txt`,ticket.join("\n"));
 }
--- core-cart.mjs
+++ core-cart.mjs
@@ -3,8 +3,87 @@ import {carts, products, promoCode, users} from "./dataset.mjs";
 import * as fs from "fs";
 import * as os from "os";
 
+Object.defineProperty(String.prototype, 'capitalize', {
+    value: function() {
+      return this.charAt(0).toUpperCase() + this.slice(1);
+    },
+    enumerable: false
+  });
+  
 const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
+
+const getHeader = () => {
+    let userInfo = os.userInfo();
+    let pid = os.getPriority();
+    return userInfo.username.toUpperCase() + " Cart - " + pid;
+};
+
+const getDate = () => {
+
+    let weekdayName = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let monthName = ["Jan", "Feb", "Mar", "Apr", "May","Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    let d = new Date();
+    
+
+    let day = d.getDate();
+    let year = d.getFullYear();
+    let weekday = weekdayName[d.getDay()]
+    let month = monthName[d.getMonth()];
+
+    return `${weekday} ${month} ${day} ${year}` ;
+};
+
+const createDir = (dirName) => {
+    fs.mkdir(dirName,
+        (err) => {
+            if (err) {
+                return console.error(err);
+            }
+            console.log('Directory created successfully!');
+        }); 
+};
+
+const createTiket = (user) => {
+    let tiketName = user + "_receipt_" + 
+    fs.writeFile(tiketName, 'bar', (err) => { if (err) throw err; });
+};
+
+const getProduct = (ean) => {
+    let product = products.find(obj => obj.ean == ean);
+    return [`[${ean}]`,product.name.toLowerCase().capitalize(),product.price];
+};
+
+const generateDecor = (decor) => {
+    return `${decor} ${"-".repeat(50)} ${decor}`;
+};
+
+const generateDiscount = (promo,total) => {
+    var discount = total*promo.percentage;
+    return [`\tSconto:\t\t\t\t${discount.toFixed(2)}\n\tTotale Scontato:\t\t\t${(total-discount).toFixed(2)}\n\n\tCODICE PROMO:\t\t\t${promo.name}`
+    ];
+};
+
+const creditUpdate = (user,total)=>{
+    var credit = user.wallet;
+    return `\t${user.firstName} ${user.lastName} ha un credito residuo di ${(credit-total).toFixed(2)}`; 
+};
+
+const createTicketName = (user) =>{
+    let d = new Date();
+    return `${user.uuid}_receipt_${d.getTime()}`;
+};
 
 export {
     discountedPrice,
+    getHeader,
+    getDate,
+    createDir,
+    getProduct,
+    generateDecor,
+    generateDiscount,
+    creditUpdate,
+    createTicketName,
 };
+
+console.log(generateDecor("*"));
